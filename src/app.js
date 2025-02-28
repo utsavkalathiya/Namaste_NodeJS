498202c223a43fdd0a3302815ee1d132a618531b
utsavXkalathiya <utsavkalathiya811@gmail.com>
2025-02-27 19:05:15 -0500

    multiple-route handlar

--- src/app.js
+++ src/app.js
@@ -2,14 +2,51 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({
-    username: "Utsav Kalathiya",
-    age: 23,
-    contry: "India",
-  });
-});
+//ab?c  This means B is optional abc and ac will route
+// ab+c => this means abbbbbbbc, b can be multiple time but a and c mendaatory
+// ab*cd means ab ANYTHNING cd
+// a(bc)?d => bc is optional
+// a(bc*)d => bc cab be as many times will work
+
+// app.get("/a(bc*)d", (req, res) => {
+//   res.send({
+//     firstName: "Utsav",
+//     lastName: "Kalathiya",
+//   });
+// });
+
+// app.post("/user", (req, res) => {
+//   res.send("Data saved successfully...");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("Data Deleted successfully...");
+// });
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    // res.send("Response!!!");
+    console.log("Response...");
+    next();
+  },
+  (req, res, next) => {
+    res.send("2nd Response!!");
+    console.log("2nd Response...");
+    next();
+  },
+  (req, res, next) => {
+    res.send("3nd Response!!");
+    console.log("3nd Response...");
+    next();
+  },
+  (req, res, next) => {
+    res.send("4nd Response!!");
+    console.log("4nd Response...");
+    next();
+  }
+);
 
 app.listen(7777, () => {
-  console.log("Server is listening successfully on 7777...");
+  console.log("Server is running....");
 });
