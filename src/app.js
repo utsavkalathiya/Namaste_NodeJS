32148d619ab093d4d4ddc604a5cd520e698e1f77
utsavXkalathiya <utsavkalathiya811@gmail.com>
2025-02-28 21:07:00 -0500

    error handlar

--- src/app.js
+++ src/app.js
@@ -2,19 +2,21 @@ const express = require("express");
 
 const app = express();
 
-const { authAdmin, userAuth } = require("./middlewares/auth");
-
-app.use("/admin", authAdmin);
-
-app.get("/user", userAuth, (req, res) => {
-  res.send("All users sent...");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong..");
+  }
 });
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data sent...");
+app.get("/getUserData", (req, res) => {
+  
+  throw new Error("asjdvcjvsd");
+  res.send("data is here..");
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("User Deleted...");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong..");
+  }
 });
 
 app.listen(7777, () => {
